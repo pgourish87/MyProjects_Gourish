3187cd0fc71cc7c545033cdc4d513617520de139
pgourish87 <pgourish012@gmail.com>
2025-01-28 10:54:00 +0530

    Kafka-Producer-application working copy.

---
+++ Kafka-Producer-example/src/main/java/com/javatechie/service/KafkaMessagePublisher.java
@@ -0,0 +1,25 @@
+package com.javatechie.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessageToTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("javatechie-demo3", message);
+        future.whenComplete((result, exception) -> {
+            if (exception == null) {
+                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("unable to send message=[" + message + "] due to: " + exception.getMessage());
+            }
+        });
+    }
+}
