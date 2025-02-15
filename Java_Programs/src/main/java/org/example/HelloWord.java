63fcb003a4967ea7d155200760d6e5f86702dd02
pgourish87 <pgourish012@gmail.com>
2025-02-15 19:44:57 +0530

    Java programs up to part-5

--- Java_Programs/src/main/java/org/example/HelloWord.java
+++ Java_Programs/src/main/java/org/example/HelloWord.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Welcome to the java world!");
     }
---
+++ Java_Programs/src/main/java/org/example/VariablesAndDataTypes.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+
+        // primitive data types
+        byte b = 10;
+        short s = 20;
+        int i = 1000;
+        long l = 1000000000L;
+        float f = 3.14f;
+        double d = 3.14;
+        char c = 'A';
+        boolean bool = true;
+        String str = "Gourish";
+
+        //Wrapper class data types
+        Byte b1 = 10;
+        Short s1 = 20;
+        Integer i1 = 1000;
+        Long l1 = 1000000000L;
+        Float f1 = 3.14f;
+        Double d1 = 3.14;
+        Character c1 = 'A';
+        Boolean bool1 = true;
+    }
+}
